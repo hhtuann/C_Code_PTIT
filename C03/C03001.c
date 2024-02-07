773b317ac4a501a57982148f520a4dcaec1810e8
Hoang Hoang Tuan <hhtuann.19@gmail.com>
2024-02-08 00:48:20 +0700

    add file

---
+++ C03/C03001.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LL long long
+#define hhtuan "hhtuann"
+#define inf int(1e9 + 7)
+#define nmax int(1e6 + 7)
+/* ...............................
+hhtuann._
+______ Hoang Hoang Tuan ______
+_ Take Off Toward Your Dream ! _
+............................... */
+int main()
+{
+    // input
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
