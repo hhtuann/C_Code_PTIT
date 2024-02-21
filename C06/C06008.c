7c5e59697a892077f711e72a346ffc669108a072
Hoang Hoang Tuan <hhtuann.19@gmail.com>
2024-02-21 12:09:06 +0700

    add file

--- C06/C06008.c
+++ C06/C06008.c
@@ -13,16 +13,6 @@ __ Take Off Toward Your Dream ! __
 int main()
 {
     // input
-    char s[1000];
-    gets(s);
-    char *t1 = strtok(s, " ");
-    printf("%s\n", t1);
-    printf("%s\n", s);
-    while (t1 != NULL)
-    {
-        t1 = strtok(NULL, " ");
-        printf("%s\n", t1);
-        printf("%s\n", s);
-    }
+
     return 0;
 }
