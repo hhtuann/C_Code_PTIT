fb5b2f88d68edfea0759f59e48ad4cb0e111c7b4
Hoang Hoang Tuan <hhtuann.19@gmail.com>
2024-02-29 02:24:00 +0700

    add file

--- C03/C03062.c
+++ C03/C03062.c
@@ -4,12 +4,14 @@
 #define hhtuan "hhtuann"
 #define inf (int)(1e9 + 7)
 #define nmax (int)(1e6 + 7)
+
 /* ...............................
 hhtuann._
 ________ Hoang Hoang Tuan ________
 __ Take Off Toward Your Dream ! __
 ............................... */
-LL gcd(LL a,int b)
+
+LL gcd(LL a, int b)
 {
     while (a * b)
     {
