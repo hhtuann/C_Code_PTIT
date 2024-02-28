2838e804ce5280b23c410f2ed92e06385a6bcf75
Hoang Hoang Tuan <hhtuann.19@gmail.com>
2024-02-29 03:54:53 +0700

    add file

--- C05/C05017.c
+++ C05/C05017.c
@@ -10,6 +10,39 @@ ________ Hoang Hoang Tuan ________
 __ Take Off Toward Your Dream ! __
 ............................... */
 int a[1005][1005];
+void solve(int *n, int *m)
+{
+    scanf("%d %d", n, m);
+    for (int i = 1; i <= *n; i++)
+    {
+        for (int j = 1; j <= *m; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    int left_x = 1; // góc trên bên trái
+    int left_y = 1;
+    int right_x = *m; // góc dưới bên phải;
+    int right_y = *n;
+    // x ngang
+    // y dọc
+    int x = (*n) * (*m);
+    while (x)
+    {
+        for (int i = left_x; i <= right_x && x > 0; i++, x--)
+            printf("%d ", a[left_y][i]);
+        for (int i = left_y + 1; i <= right_y && x > 0; i++, x--)
+            printf("%d ", a[i][right_x]);
+        for (int i = right_x - 1; i >= left_x && x > 0; i--, x--)
+            printf("%d ", a[right_y][i]);
+        for (int i = right_y - 1; i > left_y && x > 0; i--, x--)
+            printf("%d ", a[i][left_x]);
+        left_x++;
+        left_y++;
+        right_x--;
+        right_y--;
+    }
+}
 int main()
 {
     // input
@@ -18,38 +51,8 @@ int main()
     while (test--)
     {
         int n, m;
-        scanf("%d %d", &n, &m);
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= m; j++)
-            {
-                scanf("%d", &a[i][j]);
-            }
-        }
-        int left_x = 1; // góc trên bên trái
-        int left_y = 1;
-        int right_x = m; // góc dưới bên phải;
-        int right_y = n;
-        // x ngang
-        // y dọc
-        int x = n * m;
-        while (x)
-        {
-            for (int i = left_x; i <= right_x && x > 0; i++, x--)
-                printf("%d ", a[left_y][i]);
-            for (int i = left_y + 1; i <= right_y && x > 0; i++, x--)
-                printf("%d ", a[i][right_x]);
-            for (int i = right_x - 1; i >= left_x && x > 0; i--, x--)
-                printf("%d ", a[right_y][i]);
-            for (int i = right_y - 1; i > left_y && x > 0; i--, x--)
-                printf("%d ", a[i][left_x]);
-            left_x++;
-            left_y++;
-            right_x--;
-            right_y--;
-        }
+        solve(&n, &m);
         printf("\n");
     }
-
     return 0;
 }
