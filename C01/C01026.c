0269a10e01c2242864ce3a3854ff5d2a93b36ecc
Hoàng Hoàng Tuấn <155867483+hhtuann@users.noreply.github.com>
2024-03-11 11:52:23 +0700

    add file

--- C01/C01026.c
+++ C01/C01026.c
@@ -11,10 +11,8 @@ __ Take Off Toward Your Dream ! __
 ............................... */
 int isPrime(int n)
 {
-    if (n <= 1)
+    if (n < 2)
         return 0;
-    if (n < 4)
-        return 1;
     for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
