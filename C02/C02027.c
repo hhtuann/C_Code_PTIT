73471ecf1d2da71b1c31a6e8ee6596faccf679b4
Hoang Hoang Tuan <hhtuann.19@gmail.com>
2024-04-06 13:54:53 +0700

    add file

--- C02/C02027.c
+++ C02/C02027.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define LL long long
-#define hhtuan "hhtuann"
-#define inf (int)(1e9 + 7)
-#define nmax (int)(1e6 + 7)
-/* ...............................
-hhtuann._
-______ Hoang Hoang Tuan ______
-_ Take Off Toward Your Dream ! _
-............................... */
+#define hhtuann "hhtuann"
+
+// ig: hhtuann._
+// Hoang Hoang Tuan
+// Take Off Toward Your Dream !
+
 int Min(int a, int b)
 {
     return a < b ? a : b;
